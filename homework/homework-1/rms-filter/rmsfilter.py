bceaba3423c8acb9a20b2666cb5b2d5917967ca9
Alan Manuel <aloretocornidez@arizona.edu>
2023-01-27 15:32:00 -0700

    more code

--- homework/homework-1/rms-filter/rmsfilter.py
+++ homework/homework-1/rms-filter/rmsfilter.py
@@ -1,21 +1,40 @@
-
 from PIL import Image
     
 
 
 
-def kernelModification(image, kernel):
 
-    print(image, kernel)
+def kernelModification(row, column, image):
+
+    # Calculate New Pixel Value
+
+
+    pixelValue = image.getpixel((column, row))
+
+
+    # Put new pixel value in the image.
+    image.putpixel((column, row), pixelValue - 100)
 
 
 
-def convolution(width, height, image, inputKernel):
-    print(f"Inside Image with parameters: {width}, {height}, {image}")
-    for i in range(width):
-        for j in range(height):
-            pixel = image.getpixel((i, j))
-            print(f"Width: {i}\nLength: {j}\nValue: {pixel}\n\n")
+
+# Prints the pixel values in an image.
+def printImagePixelValues(image):
+    width, height = image.size
+    for column in range(width):
+        for row in range(height):
+
+            # Get Current Pixel Value
+            pixelValue = image.getpixel((column, row))
+            # print(pixelValue)
+
+
+# This function serves as a wrapper for image processing in a system.
+def imageProcessing(width, height, image):
+    # Moves the coordinate at which convolution occurs.
+    for column in range(width):
+        for row in range(height):
+            kernelModification(row, column, image)
 
 def main():
 
@@ -24,18 +43,27 @@ def main():
 
 
     # Extracting pixel map.
-    pixel_map = inputImage.load()
+    pixelMap = inputImage.load()
     
     # Extracting the width and height of the image.
     width, height = inputImage.size
 
-    print(width, height)
 
-    convolution(width, height, inputImage, kernel='')
+    
+
+    # Conducts processing on the image.
+    imageProcessing(width, height, inputImage)
 
 
+    # Print Pixel Values
+    printImagePixelValues(inputImage)
+                
 
-    print(pixel_map)
+
+    # Save New Image
+    inputImage.save('test.png')
+
+
     
 
 
@@ -43,12 +71,9 @@ def main():
 
 
 
-    # # convert an array into an image.
-    # imageFromArray = Image.fromarray(imageArray, mode='L')
+    # convert an array into an image.
+    # imageFromArray = Image.fromarray(pixelMap, mode='L')
 
-
-    # # save new image.
-    # imageFromArray.save('test.png')
 
 
 
