34adf124d4dfb1e93c13053f9b206347430132ca
Alan Manuel Loreto Cornídez <aloretocornidez@arizona.edu>
2023-01-30 10:10:53 -0700

    something

--- homework/homework-1/rms-filter/rmsfilter.py
+++ homework/homework-1/rms-filter/rmsfilter.py
@@ -10,7 +10,7 @@ def imageProcessing(width, height, image, params):
     # Moves the coordinate at which convolution occurs.
     for column in range(width):
         for row in range(height):
-            filterCalculation(row, column, image, params)
+            filterWrapperSelector(row, column, image, params)
 
 
 
@@ -27,40 +27,100 @@ def rmsFilter(row, column, image, params):
     width, height = image.size
 
 
-    ## Number of pixels contained in the kernel.
-    numberOfPixels = 0
-
     sumOfPixelValues = 0
+    totalNumberOfPixels = 0
 
-    for i in range(radius):
-        for j in range(radius):
-
-
-            
+    for horizontal in range(radius):
+        for vertical in range(radius):
 
             # pythagorian theorem for euclidian distance.
-            euclidianDistance = (i**2 + j**2) ** 1/2
+            euclidianDistance = math.sqrt(horizontal**2 + vertical**2)
+
 
             # Pixel is included in calculations if within the radius.
             if(euclidianDistance < radius):
                 
+                # set up parameters for a the function call to find the values within the radius.
+                parameters = image, row, column, horizontal, vertical 
+
+                # function returns the number of pixels that were found in the image.
+                sumOfPixelValuesInKernel, numberOfPixelsInKernel = getKernelValues(parameters)
+
+                # Add the sums in a kernel to the total sums of the image.
+                sumOfPixelValues = sumOfPixelValues + sumOfPixelValuesInKernel
+                totalNumberOfPixels = totalNumberOfPixels + numberOfPixelsInKernel
+
+                # print(sumOfPixelValues, totalNumberOfPixels)
 
 
-                image.getpixel((column, row))
+                # image.putpixel((column, row), newPixelValue)
+
+
+    newPixelValue = math.sqrt((1 / totalNumberOfPixels) * sumOfPixelValues )
+
+    newPixelValue = int(newPixelValue)
+    image.putpixel((column, row), newPixelValue)
+
+    
+                
+
+    # # Calculate New Pixel Value
+    # pixelValue = image.getpixel((column, row))
+
+    # # Put new pixel value in the image.
+    # image.putpixel((column, row), pixelValue)
 
 
 
+def getKernelValues(parameters):
 
-    # Calculate New Pixel Value
-
-
-    pixelValue = image.getpixel((column, row))
-
-
-    # Put new pixel value in the image.
-    image.putpixel((column, row), pixelValue)
+    # parse parameters
+    image, row, column, horizontal, vertical = parameters
 
 
+    # print(image.getpixel((column - horizontal, row - vertical)))
+
+    sumOfPixelValues = 0
+    numberOfPixels = 0
+
+    # calculate all new dots.
+    try:
+        value = image.getpixel((column - horizontal, row - vertical))
+    except:
+        pass
+    else:
+        sumOfPixelValues = sumOfPixelValues + value ** 2
+        numberOfPixels = numberOfPixels + 1
+        
+
+    try:
+        value = image.getpixel((column - horizontal, row + vertical))
+    except:
+        pass
+    else:
+        sumOfPixelValues = sumOfPixelValues + value ** 2
+        numberOfPixels = numberOfPixels + 1
+        
+    
+    try:
+        value = image.getpixel((column + horizontal, row - vertical))
+    except:
+        pass
+    else:
+        sumOfPixelValues = sumOfPixelValues + value ** 2
+        numberOfPixels = numberOfPixels + 1
+
+    try:
+        value = image.getpixel((column + horizontal, row + vertical))
+    except:
+        pass
+    else:
+        sumOfPixelValues = sumOfPixelValues + value ** 2
+        numberOfPixels = numberOfPixels + 1
+
+    return sumOfPixelValues, numberOfPixels
+
+    
 
 def darken(row, column, image, params):
 
@@ -74,7 +134,7 @@ def darken(row, column, image, params):
     # Put new pixel value in the image.
     image.putpixel((column, row), pixelValue - N)
 
-def filterCalculation(row, column, image, parameters):
+def filterWrapperSelector(row, column, image, parameters):
 
     # Accessing the filter type within paramters.
     filterType = parameters[0]
@@ -93,7 +153,7 @@ def printImagePixelValues(image):
 
             # Get Current Pixel Value
             pixelValue = image.getpixel((column, row))
-            # print(pixelValue)
+            print(pixelValue)
 
 
 
@@ -120,7 +180,7 @@ def main():
 
 
     # Print Pixel Values
-    printImagePixelValues(inputImage)
+    # printImagePixelValues(inputImage)
                 
 
 
