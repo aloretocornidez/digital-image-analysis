2b7f685fc5ed7ab72281ebc7f40a3c7c1b240b22
Alan Manuel <aloretocornidez@arizona.edu>
2023-03-02 11:11:43 -0700

    test

---
+++ homework/homework-3/hough-transform/README.md
@@ -0,0 +1,11 @@
+# RMS Filter
+This is an image filtering script for a grayscale image of a camera man.
+
+the `rmsfilter.py` file is a self contained module that runs the code necessary for the homework assignment.
+
+Future plans include making the script more organized by improving the CLI and adding more modularity in the main function.
+
+# Edge Detection
+This implementation uses the rms edge dection method.
+
+CLI will try to be improved.
---
+++ homework/homework-3/template.m
@@ -0,0 +1,95 @@
+% HW 3: Hough Transform To Detect Straight Lines
+
+E = imread('edges.png');
+
+% Getting the number of rows in an image.
+[rows, columns, numberOfColorChannels] = size(E);
+
+rows
+
+columns
+
+deltaTheta = 2 
+deltaRho = 2
+
+
+
+% My hough transform implementation
+[H, theta, rho] = myhough(E, deltaTheta, deltaRho);
+
+
+
+% % Matlab hough transform implementation
+% numpeaks = 3;
+% [H, theta, rho] = hough(E);
+% peaks = houghpeaks(H, numpeaks);
+% lines = houghlines(E, theta, rho, peaks);
+
+
+
+
+% imshow(-H,[]) % display the peaks as black
+% imshow(H,[])   % display the peaks as white
+% thresh = 2;
+% peaks = myhoughpeaks(H, thresh)
+% lines = myhoughlines(theta, rho, peaks)
+
+
+
+
+
+
+% %MYHOUGH Hough Transform To Calculate Hough Array
+function [H, theta, rho] = myhough(E, deltaTheta, deltaRho)
+[nrows,ncols] = size(E);
+% The theta is determined by dividing the amount of thetas in a circle
+% In this case, 360 degrees / deltaTheta
+theta = 3.1415926 / deltaTheta * 2:3.1415926 / deltaTheta * 2:3.1415926 / deltaTheta * 2;
+rho = ??:??:??;
+nTheta = length(theta);
+nRho = length(rho);
+H = zeros(nTheta,nRho);
+for r = 1:nrows
+    for c = 1:ncols
+        if E(r,c) > 0
+            for i = 0:(nTheta-1)
+                x = ??;
+                y = ??;
+                thetaval = ?? + i*??;
+                rhoval = x*?? + y*??;
+                j = floor(?? + 0.5);
+                if j >= 0 && j < nRho
+                    H(i+1,j+1) = H(i+1,j+1) + 1;
+                end
+            end
+        end
+    end
+end
+end % function
+
+
+
+%MYHOUGHPEAKS Find Peaks in Hough Array
+% function peaks = myhoughpeaks(H, thresh)
+% [nTheta,nRho] = size(H);
+% peaks = [];
+% % Add code for cases where (i,j) is along a border of the H array.
+% % Here is the code for the interior elements of the H array:
+% for i = 2:(nTheta-1)
+%     for j = 2:(nRho-1)
+%         if H(i,j) >= thresh && H(i,j) >= H(??) && H(i,j) >= H(??) && etc.
+%             peaks = [peaks; [i,j]];
+%         end
+%     end
+% end
+% end % function
+
+
+
+% %MYHOUGHLINES Determine Parameters of Lines After Hough Transform
+% function lines = myhoughlines(theta, rho, peaks)
+% lines = [];
+% for k = 1:size(peaks,1)
+%     lines = [lines; [theta(peaks(k,1)),rho(peaks(k,2))]];
+% end
+% end % function
