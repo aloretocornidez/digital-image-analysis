9f5ab5484c9e549dbe1ed00d6fa994cc1fc08396
Alan Manuel <aloretocornidez@arizona.edu>
2023-03-21 13:21:09 -0700

    adding all of the source code to start the homeowkr

---
+++ homework/homework-4/src/main.cpp
@@ -0,0 +1,28 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+ // Read the image file
+ cv::Mat image = cv::imread("address.png");
+
+ // Check for failure
+ if (image.empty()) 
+ {
+  std::cout << "Could not open or find the image" << std::endl;
+  std::cin.get(); //wait for any key press
+  return -1;
+ }
+
+ cv::String windowName = "The Guitar"; //Name of the window
+
+ cv::namedWindow(windowName); // Create a window
+
+ cv::imshow(windowName, image); // Show our image inside the created window.
+
+ cv::waitKey(0); // Wait for any keystroke in the window
+
+ cv::destroyWindow(windowName); //destroy the created window
+
+ return 0;
+}
