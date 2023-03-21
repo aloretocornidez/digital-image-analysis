e2826d904ccecb3f4173dffec7085a7e4ba63ccf
Alan Manuel <aloretocornidez@arizona.edu>
2023-03-21 16:40:08 -0700

    update homework files

--- homework/homework-4/src/main.cpp
+++ homework/homework-4/src/main.cpp
@@ -5,50 +5,57 @@ void openWindow(cv::String windowName, cv::Mat *image);
 
 int main(int argc, char **argv)
 {
+
+  // Usage for the program
+  if (argc != 2)
+  {
+    std::cout << "Usage: {programName} path/to/image" << std::endl;
+    return -1;
+  }
+
   // Read the image file
-  cv::Mat inputImage = cv::imread("address.png");
+  cv::Mat inputImage = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
 
   // Check for failure when opening the image.
   if (inputImage.empty())
   {
     std::cout << "Could not open or find the image" << std::endl;
-    std::cin.get(); // wait for any key press
     return -1;
   }
 
   // Creating an image buffer.
-  cv::Mat clone = inputImage.clone();
+  cv::Mat buffer = inputImage.clone();
 
+  cv::Mat output = inputImage.clone();
 
-
-
-
-
-
-  // scanning each pixel
-  for (int row = 0; row < clone.rows; row++)
+  // Do image processing.
+  for (int row = 0; row < buffer.rows; row++)
   {
-    for (int column = 0; column < clone.cols; column++)
+    for (int column = 0; column < buffer.cols; column++)
     {
-      int value = clone.at<cv::Vec3b>(row, column)[1];
-      std::cout << "(Row, Column): (" << row << "," << column << ")"<< "\nPixel Value: " << value <<  std::endl;
+      int pixelValue = buffer.at<cv::Vec3b>(row, column)[0]; // blue
+      // int value2 = buffer.at<cv::Vec3b>(row, column)[1]; // green
+      // int value3 = buffer.at<cv::Vec3b>(row, column)[2]; // red
 
 
+      if(pixelValue)
+
+
+      std::cout << "(Row, Column): (" << row << "," << column << ")"
+                << "\nPixel Value: " << pixelValue << std::endl; //", " << value2 << ", " << value3 << std::endl;
+
+      buffer.at<cv::Vec3b>(row, column)[0] = 200; // blue
     }
   }
 
+  cv::imwrite("test.png", buffer);
 
-
-
-
-
-
-
-
+  cv::imshow("Testing Image", cv::imread("test.png"));
+  cv::waitKey(0);
 
   // Opening the image for viewing.
   cv::String windowName = "Address"; // Name of the window
-  openWindow(windowName, &clone);
+  openWindow(windowName, &inputImage);
 
   // cv::namedWindow(windowName); // Create a window
   // cv::imshow(windowName, image); // Show our image inside the created window.
