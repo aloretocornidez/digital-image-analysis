92d262648bd585a941f42319b79b91a0ba57b586
Alan Manuel <aloretocornidez@arizona.edu>
2023-03-23 17:07:40 -0700

    adding homework 4 progress

--- homework/homework-4/src/main.cpp
+++ homework/homework-4/src/main.cpp
@@ -1,7 +1,10 @@
 #include <opencv2/opencv.hpp>
 #include "convenience.hpp"
 
-void openWindow(cv::String windowName, cv::Mat *image);
+using namespace cv;
+
+void openWindow(String windowName, Mat *image);
+// void convolution(Mat *input, Mat *output);
 
 int main(int argc, char **argv)
 {
@@ -14,7 +17,7 @@ int main(int argc, char **argv)
   }
 
   // Read the image file
-  cv::Mat inputImage = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
+  Mat inputImage = imread(argv[1], IMREAD_GRAYSCALE);
 
   // Check for failure when opening the image.
   if (inputImage.empty())
@@ -24,52 +27,98 @@ int main(int argc, char **argv)
   }
 
   // Creating an image buffer.
-  cv::Mat buffer = inputImage.clone();
+  Mat buffer = inputImage.clone();
+  Mat output = inputImage.clone();
 
-  cv::Mat output = inputImage.clone();
+  // Running otsu's method to best threshold (with the largest invariance.)
+  double maxSigma = 0;
+  int bestTheshold = -1;
 
-  // Do image processing.
+  // Test each sigma value.
+  for (int currentThreshold = 0; currentThreshold < 256; currentThreshold++)
+  {
+    std::cout << "Testing New Threshold: " << currentThreshold << std::endl;
+
+    // Parameters used in otsu's method for each new threshold.
+    double currentSigma = 0;
+    int pixelsAboveThreshold = 0;
+
+    // Get the number of row and column pixels
+    int rows = buffer.rows;
+    int columns = buffer.cols;
+
+    // Total number of pixels in the image.
+    int totalImagePixels = rows * columns;
+
+    // Scan each row and column and calculate the value.
+    for (int row = 0; row < rows; row++)
+    {
+      for (int column = 0; column < columns; column++)
+      {
+        int pixelValue = buffer.at<Vec3b>(row, column)[0];
+
+        if (pixelValue < currentThreshold)
+        {
+          output.at<Vec3b>(row, column)[0] = 255; // blue
+        }
+        else
+        {
+          output.at<Vec3b>(row, column)[0] = 0;
+        }
+      }
+    }
+
+    // Calculate the current sigma value.
+    
+
+    // Found the best threshold.
+    if (currentSigma > maxSigma)
+    {
+      maxSigma = currentSigma;
+      bestTheshold = currentThreshold;
+    }
+  }
+
+  // Use the best threshold to run binarization of the image.
   for (int row = 0; row < buffer.rows; row++)
   {
     for (int column = 0; column < buffer.cols; column++)
     {
-      int pixelValue = buffer.at<cv::Vec3b>(row, column)[0]; // blue
-      // int value2 = buffer.at<cv::Vec3b>(row, column)[1]; // green
-      // int value3 = buffer.at<cv::Vec3b>(row, column)[2]; // red
+      int pixelValue = buffer.at<Vec3b>(row, column)[0];
 
-      if (pixelValue < 100)
+      if (pixelValue < bestTheshold)
       {
-        output.at<cv::Vec3b>(row, column)[0] = 255; // blue
+        output.at<Vec3b>(row, column)[0] = 255;
       }
       else
       {
-        output.at<cv::Vec3b>(row, column)[0] = 0;
+        output.at<Vec3b>(row, column)[0] = 0;
       }
-
-      // std::cout << "(Row, Column): (" << row << "," << column << ")" << "\nPixel Value: " << pixelValue << std::endl;
     }
   }
 
-  cv::imwrite("test.png", output);
+  // Save the best image.
+  imwrite("test.png", output);
 
-  cv::Mat image = cv::imread("test.png");
+  // Opening the saved image.
+  Mat image = imread("test.png");
   openWindow("Threshold Address", &image);
 
-  // Opening the image for viewing.
-  cv::String windowName = "Address"; // Name of the window
+  // Opening the original image for viewing.
+  String windowName = "Address"; // Name of the window
   openWindow("Address Original", &inputImage);
 
   return 0;
 }
 
-void openWindow(cv::String windowName, cv::Mat *image)
+void openWindow(String windowName, Mat *image)
 {
 
-  cv::namedWindow(windowName); // Create a window
+  namedWindow(windowName); // Create a window
 
-  cv::imshow(windowName, *image); // Show our image inside the created window.
+  imshow(windowName, *image); // Show our image inside the created window.
 
-  cv::waitKey(0); // Wait for any keystroke in the window
+  waitKey(0); // Wait for any keystroke in the window
 
-  cv::destroyWindow(windowName); // destroy the created window
+  destroyWindow(windowName); // destroy the created window
 }
